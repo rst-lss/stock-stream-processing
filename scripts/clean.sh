cb171f91e91d9d5b61c91999a63a58ada5e12783
rstlss <72990855+rst-lss@users.noreply.github.com>
2025-01-23 21:57:25 +0330

    fix: the clean script is not update

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 echo "Cleaning up deployment..."
 
-kubectl delete -k src/kafka-cluster/zookeeper-deployment.yaml
-kubectl delete -k src/kafka-cluster/kafka-deployment.yaml
+kubectl delete -f src/kafka-cluster/zookeeper-deployment.yaml
+kubectl delete -f src/kafka-cluster/kafka-deployment.yaml
+kubectl delete -f src/spark-cluster/spark-master-deployment.yaml
+kubectl delete -f src/spark-cluster/spark-worker-deployment.yaml
+kubectl delete -f src/redis-database/redis-database-deployment.yaml
+kubectl delete -f src/data-ingestor/data-ingestor-deployment.yaml
+kubectl delete -f src/stream-processor/stream-processor-deployment.yaml
+kubectl delete -f src/notification/notification-deployment.yaml
+kubectl delete -f src/backend/backend-deployment.yaml
 
 read -p "Do you want to STOP Minikube container? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     minikube stop
-fi
 
-read -p "Do you want to also **DELETE** Minikube container? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    minikube stop
+    read -p "Do you want to also **DELETE** Minikube container? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        minikube delete 
+    fi
 fi
 
 echo "Cleanup completed!"
