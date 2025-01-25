b5e555e2c7f27ade6bbf7dc71ccbe87205519831
rstlss <72990855+rst-lss@users.noreply.github.com>
2025-01-25 17:18:16 +0330

    fix: stock analyzer is not starting

--- src/stock-analyzer/spark-submit-stock-analyzer.sh
+++ src/stock-analyzer/spark-submit-stock-analyzer.sh
@@ -2,7 +2,7 @@
 /opt/bitnami/spark/bin/spark-submit \
     --master spark://spark-master-service:7077 \
     --deploy-mode client \
-    --name "StreamProcessor" \
+    --name "StockAnalyzer" \
     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 \
     --driver-memory 1g \
     --conf spark.driver.maxResultSize=1g \
@@ -11,8 +11,8 @@
     --total-executor-cores 1 \
     --conf spark.executor.instances=1 \
     --conf spark.driver.bindAddress=0.0.0.0 \
-    --conf spark.driver.host=stream-processor-service \
+    --conf spark.driver.host=stock-analyzer-service \
     --conf spark.driver.port=7072 \
     --conf spark.driver.blockManager.port=35635 \
     --py-files dependencies.zip \
-    /app/stream-processor.py
+    /app/stock-analyzer.py
